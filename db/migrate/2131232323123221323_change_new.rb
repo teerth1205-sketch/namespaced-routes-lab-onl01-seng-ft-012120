c4d7d0379952b6ace85c64f32b4ae9b04b9497c5
Teerth Patel <teerth1205@gmail.com>
2020-04-07 02:47:58 +0000

    Automatically backed up by Learn

--- db/migrate/2131232323123221323_change_new.rb
+++ db/migrate/2131232323123221323_change_new.rb
@@ -1,5 +1,5 @@
 class ChangeNew < ActiveRecord::Migration
   def change
-    rename_column :preferences, :allow_new_artists, :allow_create_artists
+    rename_column :preferences, :allow_create_artists, :allow_create_artists
   end
 end
